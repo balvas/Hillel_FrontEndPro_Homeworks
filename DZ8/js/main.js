1ae23ff96a0c9a712779c1c51c5b322c9090dd79
balvas <constantine.balvas@gmail.com>
2023-02-10 15:10:46 +0000

    Add files via upload

---
+++ DZ8/js/main.js
@@ -0,0 +1,37 @@
+function removeElement() {
+    const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let itemToDelete = prompt(`Оберіть з масиву ${array} елемент на видалення`);
+
+    function arrayRemove(arr, value) {
+        return arr.filter(function (item) {
+            return item != value;
+        });
+    }
+    alert(`Новий масив ${arrayRemove(array, itemToDelete)}`);
+}
+
+function functionTwo() {
+    const array = [1, true, 'string1', 4, 'string2', 6, 7, 8, 9];
+    const filterArray = [];
+    for (let item of array) {
+        if (typeof item === 'number') {
+            filterArray.push(item);
+        }
+    }
+    let sum = filterArray.reduce(function (a, b) {
+        return a + b;
+    });
+    let averageOfNumbers = sum / filterArray.length;
+    console.log(`З масиву [${array}] вибрані числа у новий масив [${filterArray}]`)
+    console.log(`Cереднє арифметичне масиву [${filterArray}] дорівнює ${averageOfNumbers}`)
+}
+
+function functionThree() {
+    let entireString = prompt('Введіть строку', '');
+    let entireSymbols = prompt('Введіть символи для видалення зі строки через кому', '');
+    let symbolsArray = entireSymbols.split(",").map(item => item.trim());
+    for (let symbol of symbolsArray) {
+        entireString = entireString.replace(symbol, '');
+    }
+    alert(`Нова строка ${entireString}`);
+}
