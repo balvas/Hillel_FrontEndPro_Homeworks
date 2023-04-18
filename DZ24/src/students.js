0d62b21b361d575b4587c6a88bf9b27202e0afc7
balvas <constantine.balvas@gmail.com>
2023-04-18 14:12:36 +0000

    Add files via upload

---
+++ DZ24/src/students.js
@@ -0,0 +1,34 @@
+// const students = [
+//     {
+//         id: 1,
+//         name: 'John',
+//         surname: 'Doe',
+//         dateofbirth: '1999-01-01',
+//         age: 0,
+//         attendanceArray: [true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true,],
+//         estimatesArray: [100, 100, 100],
+//         academicPerformance: ''
+//     },
+//     {
+//         id: 2,
+//         name: 'George',
+//         surname: 'Smith',
+//         dateofbirth: '2000-01-01',
+//         age: 0,
+//         attendanceArray: [true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true,],
+//         estimatesArray: [100, 50, 70],
+//         academicPerformance: ''
+//     },
+//     {
+//         id: 3,
+//         name: 'John',
+//         surname: 'Smith',
+//         dateofbirth: '1995-01-01',
+//         age: 0,
+//         attendanceArray: [false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true,],
+//         estimatesArray: [100, 50, 70],
+//         academicPerformance: ''
+//     }
+// ];
+
+// export default students;
